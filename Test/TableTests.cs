225ebe30ea711ce2c01d58cdf3c1d466b724039e
ianhorswill <ian@northwestern.edu>
2017-04-15 16:23:01 -0500

    Fixed bug in TableTests
    Repeatedly ran initialization code.  That used to be non-problematic but
    is not that DefineTable throws when table is already defined.

--- Test/TableTests.cs
+++ Test/TableTests.cs
@@ -34,7 +34,7 @@ namespace Test
     [TestClass]
     public class TableTests
     {
-        public TableTests()
+        static TableTests()
         {
             KB.DefineTable("tab", 2);
             KB.AddTableRow("tab", 2, Symbol.Intern("a"), 1);
@@ -42,47 +42,7 @@ namespace Test
             KB.AddTableRow("tab", 2, Symbol.Intern("c"), 3);
             KB.AddTableRow("tab", 2, Symbol.Intern("d"), 4);
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
+        
         [TestMethod]
         public void TableOutputTest()
         {

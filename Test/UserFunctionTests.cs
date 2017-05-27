79ef0d96afce0abf686818481dbb887ec79415f6
ianhorswill <ian@northwestern.edu>
2017-05-27 15:32:29 -0500

    Expanded test suite for UserFunctions

--- Test/UserFunctionTests.cs
+++ Test/UserFunctionTests.cs
@@ -13,6 +13,13 @@ namespace Test
             TestTrue("X=int_function(937), X=938");
         }
 
+        [TestMethod,ExpectedException(typeof(ArgumentTypeException))]
+        public void IntFunctionTypeTest()
+        {
+            Functions.DeclareFunction("int_function", (int x) => x + 1);
+            TestTrue("X=int_function(937.5), X=938");
+        }
+
         [TestMethod]
         public void Int2FunctionTest()
         {
@@ -28,11 +35,32 @@ namespace Test
             TestTrue("X=float_function(937), X=938.0");
         }
 
+        [TestMethod, ExpectedException(typeof(ArgumentTypeException))]
+        public void FloatFunctionTypeTest()
+        {
+            Functions.DeclareFunction("float_function", (float x) => x + 1);
+            TestTrue("X=float_function(a), X=938.0");
+        }
+
         [TestMethod]
         public void ObjFunctionTest()
         {
-            Functions.DeclareFunction("obj_function", (object x) => x.ToString());
-            TestTrue("X=obj_function($Array), X=\"System.Array\"");
+            Functions.DeclareFunction("obj_function", (string x) => x);
+            TestTrue("X=obj_function(\"foo\"), X=\"foo\"");
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentTypeException))]
+        public void ObjFunctionTestTypeTest()
+        {
+            Functions.DeclareFunction("string_function", (string x) => x);
+            TestTrue("X=string_function(4)");
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentTypeException))]
+        public void ObjFunctionTestSubTypeTest()
+        {
+            Functions.DeclareFunction("string_function", (string x) => x);
+            TestTrue("X=string_function($Array)");
         }
     }
 }
